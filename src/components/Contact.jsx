e425963862e1a571dc03d6cce74c7c048bb8e139
Timble <nu842639@gmail.com>
2024-03-04 18:04:10 +1100

    finished content for Contact and Works, and added  file v4

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,6 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
-import { github } from '../assets';
-import { tableau } from '../assets';
-import { linkedin } from '../assets';
-import { powerBI } from '../assets';
 
 const Contact = () => {
   return (
@@ -17,51 +13,40 @@ const Contact = () => {
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
         <h4 className='text-white-100 text-[14px] pl-1 tracking-wider mt-4'>
-          dasjkh fah  fhajdfh ajdhf akdhfjkadhf akjhfkadhf kahdf kadhf uflkad fad
-          falfujfaf lahdfh 
+          Thank you taking the time for visiting my website and I hope you learned a lot
+          about me. Feel free to checkout my other links below, and please come visit again. 
         </h4>
         <h4 className='text-white-100 text-[20px] pl-1 tracking-wider mt-5'>Thank you!</h4>
 
         <div className='flex flex-row gap-6 mt-20'>
           <div>
             <a href="https://www.linkedin.com/in/timble-michael" target='_blank' rel='noopener noreferrer'>
-              <img src={linkedin} alt='Linkedin' width={30} height={30} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="currentColor"
+                style={{ color: "#ffff" }}
+                viewBox="0 0 24 24">
+                <path
+                  d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+              </svg>
             </a>
           </div>
 
           <div>
             <a href="https://github.com/tmich1997" target='_blank' rel='noopener noreferrer'>
-              <img src={github} alt='GitHub' width={30} height={30} />
-            </a>
-          </div>
-
-          <div>
-            <a href="mailto:timblemichael20@gmail.com" target='_blank' rel='noopener noreferrer'>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-7 w-7"
                 fill="currentColor"
                 style={{ color: "#fff" }}
                 viewBox="0 0 24 24">
-                <path d="M20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z" />
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
             </a>
           </div>
-
-          <div>
-            <a href="https://public.tableau.com/app/profile/timble/vizzes" target='_blank' rel='noopener noreferrer'>
-              <img src={tableau} alt='tableau'  width={30} height={30}/>
-            </a>
-          </div>
-
-          <div>
-            <a href="https://www.novypro.com/profile_projects/timble-michael" target='_blank' rel='noopener noreferrer'>
-              <img src={powerBI} alt='powrBI'  width={30} height={30}/>
-            </a>
-          </div>
-
         </div>
       </motion.div>
 
